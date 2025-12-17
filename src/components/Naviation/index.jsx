7b4af82638ca211fa9603959cd8ca54eb90bc0ca
Artur <avaprap@gmail.com>
2025-12-17 02:45:19 +0100

    fixbag in navigation

--- src/components/Naviation/index.jsx
+++ src/components/Naviation/index.jsx
@@ -2,19 +2,29 @@ import navLogo from '../../assets/navLogo.jpg';
 import menu_burger from '../../assets/menu__burger.jpg';
 import navStyle from './Navigation.module.scss';
 
-const navLinks = ['Home', 'Features', 'Pricing', 'About us', 'Contact'];
+const navLinks = [
+  { title: 'Home', href: '/' },
+  { title: 'Features', href: '/features' },
+  { title: 'Pricing', href: '/pricing' },
+  { title: 'About us', href: '/about us' },
+  { title: 'Contact', href: '/contact' },
+];
 
 const Nav = ({ setActive }) => {
   return (
     <nav className={navStyle.navigation}>
       <div className={navStyle.brand}>
-        <img className={navStyle.logo} src={navLogo} alt="navLogo" />
+        <a href="logoLinks">
+          <img className={navStyle.logo} src={navLogo} alt="navLogo" />
+        </a>
         <p className={navStyle.text}>Abstractly</p>
       </div>
       <div>
         <ul className={navStyle.links}>
           {navLinks.map((item) => (
-            <li key={item}>{item}</li>
+            <li key={item}>
+              <a href={item.href}>{item.title}</a>
+            </li>
           ))}
         </ul>
       </div>
