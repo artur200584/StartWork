df7b2ab0a58e862f98905c77496ac94cedaac1e7
Artur <avaprap@gmail.com>
2025-12-20 02:25:37 +0100

    finish

--- src/components/Naviation/index.jsx
+++ src/components/Naviation/index.jsx
@@ -3,7 +3,7 @@ import menu_burger from '../../assets/menu__burger.jpg';
 import styles from './Navigation.module.scss';
 
 const navLinks = [
-  { title: 'Home', href: '/' },
+  { title: 'Home', href: '#hero' },
   { title: 'Features', href: '#features' },
   { title: 'Pricing', href: '#pricing' },
   { title: 'About us', href: '#about us' },
