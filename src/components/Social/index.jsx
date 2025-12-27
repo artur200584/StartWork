3e0f64b78a5ec29269a6abb7660677e50039e70d
Artur <avaprap@gmail.com>
2025-12-27 19:25:16 +0100

    Add Social section with logos

--- src/components/Social/index.jsx
+++ src/components/Social/index.jsx
@@ -1,10 +1,20 @@
 import style from './Social.module.scss';
+import LogosNumbers from '../../logosNumber.jpg';
+
+const logosItem = [
+  { image: LogosNumbers, text: 'Wan Nain' },
+  //   { image: '', text: 'Wan Nain' },
+];
 
 const Social = () => {
   return (
     <section className={style.content}>
       <div className={style.text}>Used by teams that you love</div>
-      <div className={style.logos}></div>
+      <div className={style.logos}>
+        {logosItem.map((item) => {
+          return item;
+        })}
+      </div>
     </section>
   );
 };
