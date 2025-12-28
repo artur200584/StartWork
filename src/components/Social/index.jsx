049d73d20eabc129f8d8ecdc41379a88e2985e49
Artur <avaprap@gmail.com>
2025-12-28 01:47:37 +0100

    review social section

--- src/components/Social/index.jsx
+++ src/components/Social/index.jsx
@@ -1,20 +1,49 @@
+import Marquee from 'react-fast-marquee';
 import style from './Social.module.scss';
-import LogosNumbers from '../../logosNumber.jpg';
+import LogosNumber from '../../assets/logosNumber.jpg';
+import LogosArrow from '../../assets/logosArrow.jpg';
+import LogosFigure from '../../assets/logosFigure.jpg';
+import LogosDiamant from '../../assets/logosDiamant.jpg';
+import LogosCar from '../../assets/logosCar.jpg';
+import LogosMarko from '../../assets/logosMarko.jpg';
+import LogosTicket from '../../assets/logosTiket.jpg';
+import LogosAirPlane from '../../assets/logosAirPlane.jpg';
 
 const logosItem = [
-  { image: LogosNumbers, text: 'Wan Nain' },
-  //   { image: '', text: 'Wan Nain' },
+  { image: LogosNumber, text: 'Wan Nain' },
+  { image: LogosArrow, text: 'Robinwood' },
+  { image: LogosFigure, text: 'SWAPDO' },
+  { image: LogosDiamant, text: 'DIAMOND' },
+  { image: LogosCar, text: 'Air Car' },
+  { image: LogosMarko },
+  { image: LogosTicket, text: 'WIRANG' },
+  { image: LogosAirPlane, text: 'BitterFly' },
 ];
 
 const Social = () => {
   return (
     <section className={style.content}>
       <div className={style.text}>Used by teams that you love</div>
-      <div className={style.logos}>
-        {logosItem.map((item) => {
-          return item;
-        })}
-      </div>
+      <Marquee>
+        <div className={style.logos}>
+          <ul>
+            {logosItem.map((item, index) => {
+              return (
+                <>
+                  <li key={index}>
+                    <img
+                      className={style.image}
+                      src={item.image}
+                      alt="logosImage"
+                    />
+                  </li>
+                  <span>{item.text}</span>
+                </>
+              );
+            })}
+          </ul>
+        </div>
+      </Marquee>
     </section>
   );
 };
