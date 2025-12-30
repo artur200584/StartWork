44ed9cb1bcefd49e0e05085e785f7b0d6d69b963
Artur <avaprap@gmail.com>
2025-12-30 05:10:39 +0100

    fix bag with module scss

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Header from './components/Header';
 import Hero from './components/Hero';
 import Social from './components/Social';
 import Features from './components/Features';
-import style from './styles/global.scss';
+import style from './styles/global.module.scss';
 
 function App() {
   return (
