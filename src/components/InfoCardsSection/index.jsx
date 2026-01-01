83aab1aeaea6af5743d535ea9a95f97b00f7b339
Artur <avaprap@gmail.com>
2026-01-01 14:37:40 +0100

    delete ul and li

--- src/components/InfoCardsSection/index.jsx
+++ src/components/InfoCardsSection/index.jsx
@@ -3,21 +3,15 @@ import style from '../InfoCardsSection/InfoCardsSection.module.scss';
 const InfoCardsSection = ({ items }) => {
   return (
     <section className={style.content}>
-      {/* <ul> */}
       {items.map((item, index) => {
         return (
-          <div>
-            <ul>
-              <li className={style.itemsList} key={index}>
-                <img className={style.image} src={item.image} alt="" />
-                <p className={style.title}>{item.title}</p>
-                <p className={style.subtitle}>{item.subtitle}</p>
-              </li>
-            </ul>
+          <div className={style.itemsList} key={index}>
+            <img className={style.image} src={item.image} alt="" />
+            <p className={style.title}>{item.title}</p>
+            <p className={style.subtitle}>{item.subtitle}</p>
           </div>
         );
       })}
-      {/* </ul> */}
     </section>
   );
 };
